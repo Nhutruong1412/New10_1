60e17a76d5ef0821bcc97640809dcd0225ed3802
Như <truonghuynhnhu1412@gmail.com>
2022-01-23 22:28:33 +0700

    first commit

--- Scripts/Home page/Shown logo/Script1640171014446.groovy
+++ Scripts/Home page/Shown logo/Script1640171014446.groovy
@@ -25,7 +25,7 @@ WebUI.setText(findTestObject('Page_Login/input_Sign up_password'), 'TestOps!2021
 
 WebUI.click(findTestObject('Page_Login/button_Sign in'))
 
-WebUI.waitForElementPresent(findTestObject('Page_Organization Test Flag ON - Katalon TestOps/Logo'), 0)
+WebUI.waitForElementPresent(findTestObject('Page_Project - Test Cases - Katalon TestOps/Test Suites sub nav'), 0)
 
 WebUI.closeBrowser()
 
